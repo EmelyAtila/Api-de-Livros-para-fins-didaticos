79c0778d8afe9e2d2c2ea30c8b27cd91d8bfecce
EmelyAtila <emelyatila@gmail.com>
2025-09-19 18:01:12 -0300

    Inicio de Api para livros

--- src/main/java/com/emelyatila/libraryapi/model/Livro.java
+++ src/main/java/com/emelyatila/libraryapi/model/Livro.java
@@ -2,6 +2,7 @@ package com.emelyatila.libraryapi.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Entity
 @Table(name="livro")
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public class Livro {
 
     @Id
